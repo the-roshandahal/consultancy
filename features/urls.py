09a45546496211a1b85e1a41e0cf28f0e82ba65e
Roshan Dahal <58673204+the-roshandahal@users.noreply.github.com>
2023-05-24 14:24:01 +0545

    basics

---
+++ features/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.urls import re_path
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static  
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+
+
+]
--- features/views.py
+++ features/views.py
@@ -9,37 +9,40 @@ from django.urls import reverse
 
 # Create your views here.
 
+def home(request):
+    return render(request,'index.html')
 
-def add_course(request):
-	if request.method == "POST":
-		first_name = request.POST['first_name']
-		last_name = request.POST['last_name']
-		address=request.POST['address']
-		email=request.POST['email']
-		data=Course.objects.create(
-			first_name=first_name,
-			last_name=last_name,
-			address=address,
-			email=email,
-			)
-		data.save()
+
+# def add_course(request):
+# 	if request.method == "POST":
+# 		first_name = request.POST['first_name']
+# 		last_name = request.POST['last_name']
+# 		address=request.POST['address']
+# 		email=request.POST['email']
+# 		data=Course.objects.create(
+# 			first_name=first_name,
+# 			last_name=last_name,
+# 			address=address,
+# 			email=email,
+# 			)
+# 		data.save()
 		
 		
 
-	else:
-		return render (request,'student/add_student.html')
+# 	else:
+# 		return render (request,'student/add_student.html')
 
 
-def delete_course(request, course_id):
+# def delete_course(request, course_id):
     
-    course = get_object(Course, id=course_id)
+#     course = get_object(Course, id=course_id)
     
-    if request.method == 'POST':
+#     if request.method == 'POST':
      
-        course.delete()
+#         course.delete()
         
    
-        return redirect('success')
+#         return redirect('success')
     
   
-    return render(request, 'student/delete_course.html', {'course': course})
+#     return render(request, 'student/delete_course.html', {'course': course})
