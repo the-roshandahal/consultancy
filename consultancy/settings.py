492a96247d809630851d7a14d5d60a01d5c5f644
Roshan Dahal <58673204+the-roshandahal@users.noreply.github.com>
2023-12-03 22:37:50 +0545

    Update settings.py

--- consultancy/settings.py
+++ consultancy/settings.py
@@ -83,24 +83,24 @@ WSGI_APPLICATION = 'consultancy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'roshandahal_db',
-        'USER': 'roshandahal_roshandahal',
-        'PASSWORD': 'LwQVOBBNdf^[',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'roshandahal_db',
+#         'USER': 'roshandahal_roshandahal',
+#         'PASSWORD': 'LwQVOBBNdf^[',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # DATABASES = {
