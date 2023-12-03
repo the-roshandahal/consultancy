7056e4dc1660663dbf9930856ba41c3b3d59ce77
Roshan Dahal <58673204+the-roshandahal@users.noreply.github.com>
2023-12-03 22:44:26 +0545

    Update settings.py

--- consultancy/settings.py
+++ consultancy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6u!h5%_9+n!md*zd&iu_dsf(gtks=lrtzb-*rg)u)%om6txs4c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
